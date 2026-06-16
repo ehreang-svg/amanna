a04c7d9f82f8ff144f6ddbf81d7edcf3cd8e513e
ehreang-svg <ehreang@gmail.com>
2026-06-17 03:09:33 +0700

    config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwwF3BwaV3xSqAbXpu1RByl8HLcoodjeA1vlNdne7ZF4lQayg_wyjfADUJ4WTs7s1TW/exec";
-const ABSEN_API = "https://script.google.com/macros/s/AKfycbx52605R4M-KV3z0Ckv1PeXiTQ-ZAGCKMsCprx4y3vfIrU-vsLoxmKB12zCg_7B4Cz1gQ/exec";
-const TABUNGAN_API = "https://script.google.com/macros/s/AKfycbz0eLjFRei6AjsfdIkTGjJ5_TYx2qhokCfD9FnPL3wwTB3bN8Cn4_fLapwSohsbZdhu/exec";
-const JADWAL_API = "https://script.google.com/macros/s/AKfycbzE8W8Rd6coOfao7YLVZgW9-CWVPe6Nl3TQ0qy6X0r04oLbK1_brfl8yCq7HYysOSILMA/exec";
-const Quiz_API ="https://script.google.com/macros/s/AKfycbx76vMYaeEvwBdtM0sDrqFPDN7n78Z9qMVysyloix652KBctCHysJYRYyz3g2XjZWZ7/exec";
+window.API_URL = "https://script.google.com/macros/s/AKfycbwwF3BwaV3xSqAbXpu1RByl8HLcoodjeA1vlNdne7ZF4lQayg_wyjfADUJ4WTs7s1TW/exec";
+window.ABSEN_API = "https://script.google.com/macros/s/AKfycbx52605R4M-KV3z0Ckv1PeXiTQ-ZAGCKMsCprx4y3vfIrU-vsLoxmKB12zCg_7B4Cz1gQ/exec";
+window.TABUNGAN_API = "https://script.google.com/macros/s/AKfycbz0eLjFRei6AjsfdIkTGjJ5_TYx2qhokCfD9FnPL3wwTB3bN8Cn4_fLapwSohsbZdhu/exec";
+window.JADWAL_API = "https://script.google.com/macros/s/AKfycbzE8W8Rd6coOfao7YLVZgW9-CWVPe6Nl3TQ0qy6X0r04oLbK1_brfl8yCq7HYysOSILMA/exec";
+window.Quiz_API ="https://script.google.com/macros/s/AKfycbx76vMYaeEvwBdtM0sDrqFPDN7n78Z9qMVysyloix652KBctCHysJYRYyz3g2XjZWZ7/exec";
 
 const HARI_LIBUR = ["2026-01-01","2026-01-27","2026-02-19","2026-03-29","2026-04-18","2026-05-01","2026-05-12","2026-06-01","2026-08-17","2026-12-25"];
 
