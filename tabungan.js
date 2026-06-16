6a74ce98372c829b0da12474091138a86abc2a9f
ehreang-svg <ehreang@gmail.com>
2026-06-17 04:21:11 +0700

    tabungan.js

--- tabungan.js
+++ tabungan.js
@@ -9,9 +9,9 @@ async function loadKelasTabungan(){
         tabKelas.innerHTML = `<option value="">Pilih Kelas</option>`;
         kelasUnik.forEach(k=>{ tabKelas.innerHTML += `<option value="${k}">${k}</option>`; });
         tabNama.innerHTML = `<option value="">Pilih Nama Siswa</option>`;
-    }catch(err){ console.log(err); 
+    }catch(err){ console.log(err); }
 }
-
+}
 function loadNamaTabungan(){
     const siswa = dataSiswaTabungan.filter(x => x.kelas == tabKelas.value);
     tabNama.innerHTML = `<option value="">Pilih Nama Siswa</option>`;
