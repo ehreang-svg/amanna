73d7a04acc9525693a8ae0a3537c64733db5ea5c
ehreang-svg <ehreang@gmail.com>
2026-06-23 09:13:23 +0700

    dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -138,16 +138,6 @@ async function handleSubmenu(type, value, title) {
         loadRekapTabungan();
     }
 
-    if (
-    title.toUpperCase() === "DATA SISWA") {
-
-    nav("dataSiswaPage");
-
-    loadDataSiswaPage();
-
-    return;
-    }
-
     if (type === "link") {
         window.open(value, "_blank");
         return;
